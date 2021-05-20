a543955793d24e33eb93a8b4a61877e6a6146a58
Luana <1295943@sga.pucmias.br>
2021-05-19 23:39:53 -0300

    testes

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
-<?php require_once ("doors/src/main/resources/index.html") 
+<?php 
+  require_once ("doors/src/main/resources/index.html"); 
   require_once ("doors/src/main/resources/styles/estilos.css") 
 ?> 
